5bdc603ef0f404bc448a1cce406a83b610a56d97
Miguel Brondani <brondani.miguel@gmail.com>
2025-04-28 00:02:35 -0300

    versao nao concluida, porem enviada

--- t1/class_maquina.py
+++ t1/class_maquina.py
@@ -6,18 +6,16 @@ class Maquina:
         self.dicionarioTransicoes = dict()
         self._define_funcoes_transicao(estados, funcoesTransicao)
 
-        # Inicializando as três fitas
-        self.fitaEntrada = ""   # fita 1
-        self.fitaHistorico = "" # fita 2
-        self.fitaSaida = ""  # fita 3
+        self.fitaEntrada = str()    #fita 1
+        self.fitaHistorico = str()  #fita 2
+        self.fitaSaida = str()      #fita 3
 
-        # Cabeçotes para cada fita
+        #Cabecotes
         self.cabecaEntrada = 0
         self.cabecaHistorico = 0
         self.cabecaSaida = 0
 
-        # Estado atual da máquina
-        self.estadoAtual = self.estados[0]  # Primeiro estado é o inicial
+        self.estadoAtual = self.estados[0]
         self.historicoTransicoes = []
 
     def _define_funcoes_transicao(self, estados: list, funcoesTransicao: list):
@@ -27,42 +25,43 @@ class Maquina:
 
         for transicao in funcoesTransicao:
             partes = transicao.split('=')
-            condicao = partes[0].strip('()').split(',')  # Ex: (1,1) -> estado, símbolo lido
-            tripla = partes[1].strip('()').split(',')     # Ex: (3,$,R) -> próximo estado, símbolo escrito, movimento
+            condicao = partes[0].strip('()').split(',')# Ex: (1,1) -> estado, símbolo lido
+            tripla = partes[1].strip('()').split(',') # Ex: (3,$,R) -> próximo estado, símbolo escrito, movimento
             self.dicionarioTransicoes[str(condicao[0])][condicao[1]] = tripla
 
     def set_fita_entrada(self, fita: str):
-        self.fitaEntrada = fita
-        self.fitaEntrada = list(self.fitaEntrada)  # para facilitar alteração
+        self.fitaEntrada = list(fita)
         self.fitaHistorico = ['/' for _ in range(len(fita))]
         self.fitaSaida = ['/' for _ in range(len(fita))]
 
     def escreve_na_fita(self, fita, posicao, char):
-        fita_lista = list(fita)  # Converte a fita para lista
+        fita_lista = list(fita)
         while posicao >= len(fita_lista):
-            fita_lista.append('_')  # Adiciona espaços em branco até atingir a posição necessária
-        fita_lista[posicao] = char  # Agora é seguro escrever na posição
-        return ''.join(fita_lista)  # Converte de volta para string
-
+            fita_lista.append('_')
+        fita_lista[posicao] = char  
+        return ''.join(fita_lista)  
+    
     def move_cabecote(self, movimento: str, fita: str):
         if movimento == 'R':
-            if fita == 'input':
+            if fita == 'entrada':
                 self.cabecaEntrada += 1
-            elif fita == 'history':
+            elif fita == 'historico':
                 self.cabecaHistorico += 1
-            elif fita == 'output':
+            elif fita == 'saida':
                 self.cabecaSaida += 1
+
+
         elif movimento == 'L':
-            if fita == 'input':
+            if fita == 'entrada':
                 self.cabecaEntrada -= 1
-            elif fita == 'history':
+            elif fita == 'historico':
                 self.cabecaHistorico -= 1
-            elif fita == 'output':
+            elif fita == 'saida':
                 self.cabecaSaida -= 1
 
     def _garante_espaco_na_fita(self, fita, posicao):
         while posicao >= len(fita):
-            fita += '_'  # Adiciona espaços em branco (ou qualquer outro símbolo para espaço) na fita
+            fita += '_'  # Adiciona espaços em branco
         return fita
 
     def processamento_main(self):
@@ -79,25 +78,18 @@ class Maquina:
             self.historicoTransicoes.append({
                 "estadoAnterior": self.estadoAtual,
                 "cabecaEntradaAnterior": self.cabecaEntrada,
-                "simboloInputAnterior": charLido,
-                "simboloHistoryAnterior": self.fitaHistorico[self.cabecaHistorico]
+                "simboloentradaAnterior": charLido,
+                "simbolohistoricoAnterior": self.fitaHistorico[self.cabecaHistorico]
             })
 
-            # Atualiza estado
             self.estadoAtual = tripla[0]
-
-            # Escreve símbolo na fita de entrada
             self.fitaEntrada = self.escreve_na_fita(self.fitaEntrada, self.cabecaEntrada, tripla[1])
-
-            # Registra movimento na fita de história (grava o símbolo anterior)
             self.fitaHistorico = self.escreve_na_fita(self.fitaHistorico, self.cabecaHistorico, charLido)
 
-            # Move cabeçotes
-            self.move_cabecote(tripla[2], 'input')
-            self.move_cabecote(tripla[2], 'history')
+            self.move_cabecote(tripla[2], 'entrada')
+            self.move_cabecote(tripla[2], 'historico')
 
     def _copiar_saida(self):
-        # Cria novos estados Bs para copiar input para output
         self.estadoAtual = 'B0'
         self.cabecaEntrada = 0
         self.cabecaSaida = 0
@@ -105,27 +97,20 @@ class Maquina:
         while self.cabecaEntrada < len(self.fitaEntrada):
             simbolo = self.fitaEntrada[self.cabecaEntrada]
 
-            # Copia símbolo da fita input para a output
             self.fitaSaida = self.escreve_na_fita(self.fitaSaida, self.cabecaSaida, simbolo)
 
-            # Move apenas input e output (history parado)
-            self.move_cabecote('R', 'input')
-            self.move_cabecote('R', 'output')
+            self.move_cabecote('R', 'entrada')
+            self.move_cabecote('R', 'saida')
 
     def _retrace(self):
-        # Inverte as transições
         self.estadoAtual = self.estados[-1]  # Começa do estado final
         for transicao in reversed(self.historicoTransicoes):
-            # A transição inversa envolve a troca do estado e do símbolo lido
             self.estadoAtual = transicao["estadoAnterior"]
             self.cabecaEntrada = transicao["cabecaEntradaAnterior"]
-            self.fitaEntrada = self.escreve_na_fita(self.fitaEntrada, self.cabecaEntrada, transicao["simboloInputAnterior"])
-            self.fitaHistorico = self.escreve_na_fita(self.fitaHistorico, self.cabecaHistorico, transicao["simboloHistoryAnterior"])
-
-            # Move os cabeçotes para o movimento inverso
-            # Aqui, você pode precisar de mais lógica para lidar com o movimento da fita de entrada e histórico de forma reversa
+            self.fitaEntrada = self.escreve_na_fita(self.fitaEntrada, self.cabecaEntrada, transicao["simboloentradaAnterior"])
+            self.fitaHistorico = self.escreve_na_fita(self.fitaHistorico, self.cabecaHistorico, transicao["simbolohistoricoAnterior"])
 
     def mostrar_fitas(self):
-        print("Fita Input: ", "".join(self.fitaEntrada))
-        print("Fita History: ", "".join(self.fitaHistorico))
-        print("Fita Output: ", "".join(self.fitaSaida))
+        print(f"Fita entrada: {self.fitaEntrada}")
+        print(f"Fita historico: {self.fitaHistorico}")
+        print(f"Fita saida: {self.fitaSaida}")
--- t1/main.py
+++ t1/main.py
@@ -8,7 +8,8 @@ def le_arquivo(path):
             "estados": linhas[1].split(),
             "alfabetoEntrada": linhas[2].split(),
             "alfabetoFita": linhas[3].split(),
-            "funcoesTransicao": linhas[4:-1]
+            "funcoesTransicao": linhas[4:-1],
+            "fitaEntrada": linhas[-1]
         }
         return definicaoMaquina
 
@@ -22,12 +23,9 @@ if __name__ == "__main__":
         definicoes['funcoesTransicao']
     )
 
-    # Definir a fita de entrada com um exemplo, conforme o arquivo de entrada
-    fitaInicial = '1100'  # Exemplo de entrada para a máquina
+    fitaInicial = definicoes['fitaEntrada'] 
     turing.set_fita_entrada(fitaInicial)
 
-    # Processar a máquina de Turing reversível
     turing.processamento_main()
 
-    # Mostrar o estado das três fitas após a execução
     turing.mostrar_fitas()
